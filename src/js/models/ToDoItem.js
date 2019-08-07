2828ee89b7c4880a194913010fc83fb53a32756e
sushmavara ravikumar <sushmavararavikumar@sushmavaras-MacBook-Pro.local>
2019-08-07 14:04:23 +0530

    adding functions to prototype

--- src/js/models/ToDoItem.js
+++ src/js/models/ToDoItem.js
@@ -1,36 +1,33 @@
 import {todoItemTemplate} from '../views/Todo-App-Templates'
-export default class ToDoItem {
 
-    constructor(title,description,dueDate)
-    {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.id = Date.now();
-        this.isCompleted = false;
-    }
+export function ToDoItem(title,description,dueDate){
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.id = Date.now();
+    this.isCompleted = false;
+}
 
-    markItemComplete() {
-        this.isCompleted = true;
-    }
+ToDoItem.prototype.markItemComplete = function(){
+    this.isCompleted = true;
+    return this;
+}
 
-    toggleMarkComplete () {
-        this.isCompleted = !this.isCompleted;
-        return this;
-    }
+ToDoItem.prototype.toggleMarkComplete = function(){
+    this.isCompleted = !this.isCompleted;
+    return this;
+}
 
-    editTodoItem (editedTodo) {
-        this.title = editedTodo.title;
-        this.description = editedTodo.description;
-        this.dueDate = editedTodo.dueDate; 
-        return this;    
-    }
+ToDoItem.prototype.editTodoItem = function(editedTodo){
+    this.title = editedTodo.title;
+    this.description = editedTodo.description;
+    this.dueDate = editedTodo.dueDate; 
+    return this;  
+}
 
-    createTodoItemNode()
-    {
-        return todoItemTemplate.replace("%id%",this.id).
+ToDoItem.prototype.createTodoItemNode = function(){
+    return todoItemTemplate.replace("%id%",this.id).
         replace("%title%",this.title).
         replace("%description%",this.description).
         replace("%due-date%",this.dueDate);
-    }
 }
