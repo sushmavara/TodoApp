d1e23e44f8d799ac89ae992db2dd731f2947fa47
sushmavara ravikumar <sushmavararavikumar@sushmavaras-MacBook-Pro.local>
2019-08-30 13:03:20 +0530

    latest

--- src/js/models/ToDoItem.js
+++ src/js/models/ToDoItem.js
@@ -10,24 +10,20 @@ function ToDoItem(toDoItemInfo){
 
 ToDoItem.prototype.markTodoComplete = function(){
     this.isCompleted = true;
-    return this;
 }
 
 ToDoItem.prototype.toggleMarkComplete = function(){
     this.isCompleted = !this.isCompleted;
-    return this;
 }
 
 ToDoItem.prototype.editTodoItem = function(editedTodo){
     this.title = editedTodo.title;
     this.description = editedTodo.description;
     this.dueDate = editedTodo.dueDate; 
-    return this;  
 }
 
 ToDoItem.prototype.toggleMarkChecked= function(){
     this.isChecked = !this.isChecked;
-    return this;
 }
 
 export default ToDoItem;
